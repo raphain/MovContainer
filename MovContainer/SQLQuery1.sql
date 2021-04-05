44ef3e8fdbf2951f486d1039c1fa255600a3fec7
Raphain <raphain1@hotmail.com>
2021-04-05 17:17:35 -0300

    second commit

--- MovContainer/SQLQuery1.sql
+++ MovContainer/SQLQuery1.sql
@@ -6,6 +6,3 @@ FROM Movimentations m inner join Containers c on m.ContainerId = c.Id
 inner join ContainerCategories cc on c.ContainerCategoryId = cc.Id
 inner join MovTypes mt on mt.Id = m.MovTypeId
 order by c.Client;
-
-
-Select * from Movimentations;
